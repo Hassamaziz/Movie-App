6828d3a38a021d8d8892716b38f54ef36ab3ac02
hassamaziz <hassamaziz001@gmail.com>
2025-05-01 23:03:12 +0500

    real work begins

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,31 +1,11 @@
-import "./App.css";
-import { useEffect, useState } from "react";
+import React from 'react'
 
-const Card = (props) => {
-  const [hasLiked , setHasLiked] = useState(false);
-  useEffect(()=>{
-console.log(`You have ${hasLiked ? "liked" : "not liked"} ${props.title}`);
-
-  });
-  return (
-    <div className="card">
-      <h2>{props.title}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-
-      </button>
-    </div>
-  );
-};
 const App = () => {
-  
   return (
-    <div className="card-container">
-      <Card title="Avatar" />
-      <Card title="Spider-Man" />
-      <Card title="The Batman" />
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
