ee766278d6e4aa5e84f518642f545c6e3559e081
hassamaziz <hassamaziz001@gmail.com>
2025-05-03 00:03:36 +0500

    Showing the Movies

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -14,17 +14,42 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState(""); 
   const [errorMessage,setErrorMessage] =useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
 
-  const fetchMovies = async (searchTerm) => {
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
    try { 
+    const endpoint = `${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+    const response = await fetch(endpoint , API_OPTIONS);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+
+    if (data.response === "False") {
+      setErrorMessage(data.Error || "Movie not found");
+      setMovieList([]);
+      return;
+    }
+    setMovieList(data.results || []);
+    setIsLoading(false);
+    
   
    } catch (error) {
-    console.log("Error fetching Movies"+error);
+    console.log("Error fetching Movies" + error);
     setErrorMessage("Error fetching movies. Please try again later.");
    }
+    finally {
+      setIsLoading(false);
+    }
   }
 
+
+
   useEffect(() => {
+    fetchMovies();
     
   }, [])
   
@@ -43,7 +68,26 @@ const App = () => {
 
         <section className="all-movies">
         <h2>All Movies</h2>
-        {errorMessage && <p className="error">{errorMessage}</p>}
+         
+          {isloading ? (
+           <p className="text-white">Loading...</p>
+          ) : errorMessage ? (
+            <div className="text-red-600">{errorMessage}</div>
+          ) : movieList.length > 0 ? (
+            <ul className="movie-list">
+              {movieList.map((movie) => (
+                <li key={movie.id}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                  <h3 className="text-white">{movie.title}</h3>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="no-movies">No movies found.</div>
+          )}
         
         </section>
       </div>
